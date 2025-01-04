bcb730ee91d06a3d2cf7e2e80393e06955e736e6
LokeshN1 <lokeshnegi399@gmail.com>
2025-01-05 01:09:54 +0530

    feat(auth): implement login, signup, and logout functionality
    - Added frontend pages for login, signup, and logout.
    - Integrated authentication store in src/store/.
    - Updated components to handle user authentication states.
    - Configured Tailwind CSS and PostCSS for consistent styling.
    - Improved folder structure with src/components/ and src/lib/.
    
    This commit sets up the foundational authentication features for the app.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,17 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
+import cors from "cors";
+
 dotenv.config();        // so we can use env varibles from .env file
 const app = express();
 app.use(express.json()); // so we can parse json data from request body
 app.use(cookieParser()); // so we can parse cookies from request headers
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
